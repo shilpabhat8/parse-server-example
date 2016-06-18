683e1c4f9b7b0235815e1572cca54dad27eec563
shilpabhat8 <shilpab8@gmail.com>
2016-06-19 02:56:00 +0530

    Twilio

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,5 @@
 
 Parse.Cloud.define('hello', function(req, res) {
+  var twilio = require('twilio')('AC1c5d074299795e11907706bc8c34325d', '4293afaf04268d42c02aabaf2237b67f');
   res.success('Hi');
 });
